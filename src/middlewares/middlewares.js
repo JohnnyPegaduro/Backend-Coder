1140ed0b899329b22a2b7f4be895735a14d6adf3
JohnnyPegaduro <matiasdeluca2000@gmail.com>
2022-10-03 16:34:00 -0300

    middlewares, models & routes

---
+++ src/middlewares/middlewares.js
@@ -0,0 +1,39 @@
+const middlewares = {};
+
+//? completedFields revisará si el input del formulario o la query recibe todos los parámetros solicitados // Método POST
+
+middlewares.completedFields = (req, res, next) => {
+    const {
+        title,
+        price,
+        thumbnail,
+        description,
+        code,
+        stock
+    } = req.body;
+    title && price && thumbnail && description && code && stock ?
+        next() :
+        res.status(300).send({
+            message: "Debe completar todos los campos"
+        });
+};
+
+//? Permisos de administrador
+
+//TODO implementar otros roles
+
+middlewares.adminAuth = (permissions) => {
+
+    return (req, res, next) => {
+        permissions === true ?
+            next() :
+            res
+            .status(401)
+            .json({
+                error: -1,
+                description: "unauthorized permission"
+            });
+    };
+};
+
+module.exports = middlewares;
